543e097bbd4c9ca2859a178f5678eadf365667b6
qlereboursBS <quentin.lerebours@bearstudio.fr>
2022-12-29 11:09:59 +0700

    feat: Added empty features pages along with WorkInProgress.tsx component (#17)

---
+++ components/WorkInProgress.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Callout } from 'nextra-theme-docs'
+
+
+export const WorkInProgress = () => (
+  <Callout type="info" emoji="ℹ️">
+    The documentation is currently being written by the community.
+    Feel free to{' '}
+    <a
+      href="https://github.com/BearStudio/start-ui-web-doc"
+      target="_blank"
+      rel="noreferrer noopener"
+      style={{ textDecoration: 'underline' }}
+    >
+      contribute ↗
+    </a>
+  </Callout>
+)
