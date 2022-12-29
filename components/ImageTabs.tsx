c488211137cffb282edd39e6dea48ec934381071
qlereboursBS <quentin.lerebours@bearstudio.fr>
2022-12-29 10:16:22 +0700

    Document authentication features: Sign up, login and reset password flow (#1)
    * feat: Document authentication features: Sign up, login, reset password flow and session's expiration

---
+++ components/ImageTabs.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Tab, Tabs } from 'nextra-theme-docs'
+import Image from "next/image";
+import { isMobile } from 'react-device-detect';
+
+export const ImageTabs: React.FC<ImageTabsProps> = ({
+                                                      desktopImage,
+                                                      mobileImage,
+                                                    }) => {
+
+  const tabs = [
+    desktopImage ? 'Desktop' : null,
+    mobileImage ? 'Mobile' : null
+  ].filter(device => !!device);
+
+  return (
+    <Tabs items={tabs}>
+      {!!desktopImage?.src && (
+        <Tab>
+          <Image {...desktopImage} placeholder="blur" />
+        </Tab>
+      )}
+      {!!mobileImage?.src && (
+        <Tab>
+          {/* We add a max height only on desktop */}
+          <Image {...mobileImage} height={!isMobile ? 500 : undefined} placeholder="blur" />
+        </Tab>
+      )}
+    </Tabs>
+  )
+}
+
+type ImageProps = {
+  src: string,
+  alt: string,
+  width?: number,
+  height?: number,
+};
+
+type ImageTabsProps = {
+  desktopImage?: ImageProps,
+  mobileImage?: ImageProps,
+};
