1493378bd0fcb94c206e5c5c3ff87cffa4439f44
Hugo Pérard <hugo.perard@bearstudio.fr>
2023-01-06 13:02:34 +0700

    feat: absolute paths for pages, screenshots and components

--- components/ImageTabs.tsx
+++ components/ImageTabs.tsx
@@ -1,17 +1,16 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Tab, Tabs } from 'nextra-theme-docs'
+import React from "react";
+import { Tab, Tabs } from "nextra-theme-docs";
 import Image from "next/image";
-import { isMobile } from 'react-device-detect';
+import { isMobile } from "react-device-detect";
 
 export const ImageTabs: React.FC<ImageTabsProps> = ({
-                                                      desktopImage,
-                                                      mobileImage,
-                                                    }) => {
-
+  desktopImage,
+  mobileImage,
+}) => {
   const tabs = [
-    desktopImage ? 'Desktop' : null,
-    mobileImage ? 'Mobile' : null
-  ].filter(device => !!device);
+    desktopImage ? "Desktop" : null,
+    mobileImage ? "Mobile" : null,
+  ].filter((device) => !!device);
 
   return (
     <Tabs items={tabs}>
@@ -23,21 +22,25 @@ export const ImageTabs: React.FC<ImageTabsProps> = ({
       {!!mobileImage?.src && (
         <Tab>
           {/* We add a max height only on desktop */}
-          <Image {...mobileImage} height={!isMobile ? 500 : undefined} placeholder="blur" />
+          <Image
+            {...mobileImage}
+            height={!isMobile ? 500 : undefined}
+            placeholder="blur"
+          />
         </Tab>
       )}
     </Tabs>
-  )
-}
+  );
+};
 
 type ImageProps = {
-  src: string,
-  alt: string,
-  width?: number,
-  height?: number,
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
 };
 
 type ImageTabsProps = {
-  desktopImage?: ImageProps,
-  mobileImage?: ImageProps,
+  desktopImage?: ImageProps;
+  mobileImage?: ImageProps;
 };
