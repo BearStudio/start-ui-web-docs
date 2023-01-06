4a9ffc098b93ec8199230c9cd4cc8b90810d701e
Hugo Pérard <hugo.perard@bearstudio.fr>
2023-01-06 14:32:00 +0700

    feat: add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://docs.web.start-ui.com",
+  generateRobotsTxt: true,
+  // ...other options
+};
