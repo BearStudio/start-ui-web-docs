ae054d6bbbb88ad9cf0a8b65199c57f681d1ddd6
Yoann Fleury <yoann.fleury@yahoo.com>
2023-01-06 17:02:41 +0100

    fix: add VERCEL_URL to the next sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://docs.web.start-ui.com",
+  siteUrl:
+    `https://${process.env.VERCEL_URL}` ||
+    process.env.SITE_URL ||
+    "https://docs.web.start-ui.com",
   generateRobotsTxt: true,
 };
