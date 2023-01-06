340a9ca32a1b30dbebbffbf55dea87389642ce39
Hugo Pérard <hugo.perard@bearstudio.fr>
2023-01-06 13:38:53 +0700

    feat(ImageTabs): global state for displayed image in tabs

--- components/ImageTabs.tsx
+++ components/ImageTabs.tsx
@@ -2,18 +2,30 @@ import React from "react";
 import { Tab, Tabs } from "nextra-theme-docs";
 import Image, { ImageProps as NextImageProps } from "next/image";
 import { isMobile } from "react-device-detect";
+import { useAppContext } from "@/_app";
 
 export const ImageTabs: React.FC<ImageTabsProps> = ({
   desktopImage,
   mobileImage,
 }) => {
-  const tabs = [
-    desktopImage ? "Desktop" : null,
-    mobileImage ? "Mobile" : null,
-  ].filter((device) => !!device);
+  const { displayedImage, setDisplayedImage } = useAppContext();
+
+  const tabs = (
+    [desktopImage ? "Desktop" : null, mobileImage ? "Mobile" : null] as const
+  ).filter((device) => !!device);
+
+  const selectedIndex = tabs.findIndex(
+    (tab) => tab.toLowerCase() === displayedImage
+  );
+  const selectIndex = (newIndex: number) =>
+    setDisplayedImage(
+      tabs.find((_, index) => index === newIndex).toLowerCase() as
+        | "desktop"
+        | "mobile"
+    );
 
   return (
-    <Tabs items={tabs}>
+    <Tabs items={tabs} selectedIndex={selectedIndex} onChange={selectIndex}>
       {!!desktopImage?.src && (
         <Tab>
           <Image {...desktopImage} placeholder="blur" />
