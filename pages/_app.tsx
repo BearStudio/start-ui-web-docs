61da5f760e7d27906f0574ab016f16b0b2f520c3
Yoann Fleury <yoann.fleury@yahoo.com>
2023-05-26 12:16:12 +0200

    feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import { ImageTabsContextProvider } from "@/components/ImageTabs";
 import { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ImageTabsContextProvider>
-      <Component {...pageProps} />
-    </ImageTabsContextProvider>
+    <>
+      <ImageTabsContextProvider>
+        <Component {...pageProps} />
+      </ImageTabsContextProvider>
+      <Analytics />
+    </>
   );
 };
 
