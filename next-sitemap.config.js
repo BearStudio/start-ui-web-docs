281ebecac156ebf60989e644be81d615f3d101d5
Yoann Fleury <yoann.fleury@yahoo.com>
2023-05-30 23:00:31 +0200

    fix: sitemap using SITE_URL for production, VERCEL_URL for others

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl:
-    `https://${process.env.VERCEL_URL}` ||
     process.env.SITE_URL ||
+    `https://${process.env.VERCEL_URL}` ||
     "https://docs.web.start-ui.com",
   generateRobotsTxt: true,
 };
