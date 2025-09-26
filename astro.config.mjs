3a31f36301fa73f410228f5d13dc7d3717a2d392
Noé TATOUD <noe@bearstudio.fr>
2025-09-26 18:08:22 +0200

    feat(v3): Add header metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,82 @@ export default defineConfig({
         replacesTitle: true,
       },
 
+      head: [
+        {
+          tag: "meta",
+          attrs: { name: "msapplication-TileColor", content: "#000000" },
+        },
+        { tag: "meta", attrs: { name: "theme-color", content: "#000000" } },
+        {
+          tag: "meta",
+          attrs: {
+            name: "viewport",
+            content: "width=device-width, initial-scale=1.0",
+          },
+        },
+        {
+          tag: "meta",
+          attrs: { "http-equiv": "Content-Language", content: "en" },
+        },
+        {
+          tag: "meta",
+          attrs: {
+            name: "description",
+            content:
+              "Documentation of Start UI [web], an opinionated UI starter for React. From Bearstudio team",
+          },
+        },
+        {
+          tag: "meta",
+          attrs: {
+            name: "og:description",
+            content:
+              "Documentation of Start UI [web], an opinionated UI starter for React. From Bearstudio team",
+          },
+        },
+        {
+          tag: "meta",
+          attrs: { name: "twitter:card", content: "summary_large_image" },
+        },
+        {
+          tag: "meta",
+          attrs: {
+            name: "twitter:image",
+            content:
+              "https://github.com/BearStudio/start-ui-web/raw/master/assets/thumbnail.png",
+          },
+        },
+        {
+          tag: "meta",
+          attrs: {
+            name: "twitter:site:domain",
+            content: "https://github.com/bearstudio/start-ui-web-doc",
+          },
+        },
+        {
+          tag: "meta",
+          attrs: {
+            name: "twitter:url",
+            content: "https://github.com/bearstudio/start-ui-web-doc",
+          },
+        },
+        {
+          tag: "meta",
+          attrs: { name: "og:title", content: "Start UI [web] documentation" },
+        },
+        {
+          tag: "meta",
+          attrs: {
+            name: "og:image",
+            content:
+              "https://github.com/BearStudio/start-ui-web/raw/master/assets/thumbnail.png",
+          },
+        },
+        {
+          tag: "link",
+          attrs: { rel: "icon", href: "/favicon-16x16.png", type: "image/png" },
+        },
+      ],
       components: {
         SocialIcons: "./src/components/overrides/SocialIcons.astro",
         Footer: "./src/components/overrides/Footer.astro",
